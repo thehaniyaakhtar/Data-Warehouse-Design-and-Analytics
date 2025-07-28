91414865f26d11100ebb69bb1804c84df83366ec
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:22:54 +0530

    Update 00_init_database.sql

--- scripts/EDA/00_init_database.sql
+++ scripts/EDA/00_init_database.sql
@@ -1,3 +1,17 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouseAnalytics' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, this script creates a schema called gold
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouseAnalytics' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
 DROP database IF EXISTS DataWarehouseAnalytics;
 CREATE DATABASE DataWarehouseAnalytics;
 USE DataWarehouseAnalytics;
