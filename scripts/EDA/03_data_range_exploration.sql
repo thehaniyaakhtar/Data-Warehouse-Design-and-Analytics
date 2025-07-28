6302126d8ce65ae425f96f35ee52833364dbc8bc
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:23:44 +0530

    Update 03_data_range_exploration.sql

--- scripts/EDA/03_data_range_exploration.sql
+++ scripts/EDA/03_data_range_exploration.sql
@@ -1,3 +1,19 @@
+
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+
+-- Retrieve a list of unique countries from which customers originate
+
 SELECT * 
 FROM gold_fact_sales
 ;
