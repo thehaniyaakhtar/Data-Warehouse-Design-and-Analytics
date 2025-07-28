b1261c570650466c0662ee84a4ada76f29d1f228
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:25:27 +0530

    Update 06_ranking_analysis.sql

--- scripts/EDA/06_ranking_analysis.sql
+++ scripts/EDA/06_ranking_analysis.sql
@@ -1,4 +1,16 @@
+===============================================================================
+Ranking Analysis
+===============================================================================
+Purpose:
+    - To rank items (e.g., products, customers) based on performance or other metrics.
+    - To identify top performers or laggards.
 
+SQL Functions Used:
+    - Window Ranking Functions: RANK(), DENSE_RANK(), ROW_NUMBER(), TOP
+    - Clauses: GROUP BY, ORDER BY
+===============================================================================
+*/
+	
 SELECT * 
 FROM gold_dim_customers;
 
