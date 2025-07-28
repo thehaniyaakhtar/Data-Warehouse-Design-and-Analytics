d6b90d722c4b97dfa014d1ffa52575f5e0176f6f
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:38:00 +0530

    Update 11_part_to_whole_analysis.sql

--- scripts/EDA/11_part_to_whole_analysis.sql
+++ scripts/EDA/11_part_to_whole_analysis.sql
@@ -1,3 +1,18 @@
+/*
+===============================================================================
+Part-to-Whole Analysis
+===============================================================================
+Purpose:
+    - To compare performance or metrics across dimensions or time periods.
+    - To evaluate differences between categories.
+    - Useful for A/B testing or regional comparisons.
+
+SQL Functions Used:
+    - SUM(), AVG(): Aggregates values for comparison.
+    - Window Functions: SUM() OVER() for total calculations.
+===============================================================================
+*/
+
 WITH category_sales AS (
 	SELECT  
 		p.category,
