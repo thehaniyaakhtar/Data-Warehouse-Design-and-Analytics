535760c00382a35ede97d03b7bd2b5e7dcc7c584
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:24:39 +0530

    Update 04_measures_exploration.sql

--- scripts/EDA/04_measures_exploration.sql
+++ scripts/EDA/04_measures_exploration.sql
@@ -1,3 +1,16 @@
+/*
+===============================================================================
+Measures Exploration (Key Metrics)
+===============================================================================
+Purpose:
+    - To calculate aggregated metrics (e.g., totals, averages) for quick insights.
+    - To identify overall trends or spot anomalies.
+
+SQL Functions Used:
+    - COUNT(), SUM(), AVG()
+===============================================================================
+*/
+
 SELECT *
 FROM gold.fact_sales;
 
