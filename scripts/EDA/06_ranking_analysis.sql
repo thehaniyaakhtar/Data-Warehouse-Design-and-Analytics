f5a68eb05a1d65e9b2c79f84c50d0e7389489750
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:14:32 +0530

    Update 06_ranking_analysis.sql

--- scripts/EDA/06_ranking_analysis.sql
+++ scripts/EDA/06_ranking_analysis.sql
@@ -1,1 +1,52 @@
 
+SELECT * 
+FROM gold_dim_customers;
+
+SELECT * 
+FROM gold_dim_products;
+
+SELECT p.product_name,
+       SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+    ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC;
+
+SELECT p.product_name,
+       SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+    ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue;
+
+SELECT *
+FROM (
+    SELECT
+        p.product_name,
+        SUM(f.sales_amount) AS total_revenue,
+        RANK() OVER (ORDER BY SUM(f.sales_amount) DESC) AS rank_products
+    FROM gold.fact_sales f
+    LEFT JOIN gold.dim_products p
+        ON p.product_key = f.product_key
+    GROUP BY p.product_name
+) AS ranked_products
+WHERE rank_products <= 5;
+
+
+;
+	
+SELECT c.customer_key,
+    c.first_name,
+    c.last_name,
+    SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+    ON c.customer_key = f.customer_key
+GROUP BY 
+    c.customer_key,
+    c.first_name,
+    c.last_name
+ORDER BY total_revenue DESC
+LIMIT 10;
