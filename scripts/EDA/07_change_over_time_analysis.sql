9fb08f8935224e6b42fcd1c34374cfd1c56d4fab
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:26:24 +0530

    Update 07_change_over_time_analysis.sql

--- scripts/EDA/07_change_over_time_analysis.sql
+++ scripts/EDA/07_change_over_time_analysis.sql
@@ -1,3 +1,18 @@
+/*
+===============================================================================
+Change Over Time Analysis
+===============================================================================
+Purpose:
+    - To track trends, growth, and changes in key metrics over time.
+    - For time-series analysis and identifying seasonality.
+    - To measure growth or decline over specific periods.
+
+SQL Functions Used:
+    - Date Functions: DATEPART(), DATETRUNC(), FORMAT()
+    - Aggregate Functions: SUM(), COUNT(), AVG()
+===============================================================================
+*/
+
 SELECT
 	YEAR(order_date) As order_year,
     MONTH(order_date) AS order_month,
