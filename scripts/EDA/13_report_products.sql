31c1dc0532bd973e8b2f7f7a18394b1fb7ea3ee0
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:17:30 +0530

    Update 13_report_products.sql

--- scripts/EDA/13_report_products.sql
+++ scripts/EDA/13_report_products.sql
@@ -1,1 +1,82 @@
+DROP VIEW IF EXISTS gold_report_products;
+
+CREATE VIEW gold_report_products AS
+
+WITH base_query AS (
+    SELECT
+        f.order_number,
+        f.order_date,
+        f.customer_key,
+        f.sales_amount,
+        f.quantity,
+        p.product_key,
+        p.product_name,
+        p.category,
+        p.subcategory,
+        p.cost
+    FROM gold_fact_sales f
+    LEFT JOIN gold_dim_products p ON f.product_key = p.product_key
+    WHERE f.order_date IS NOT NULL
+),
+
+product_aggregations AS (
+    -- Step 2: Aggregate metrics per product
+    SELECT
+        product_key,
+        product_name,
+        category,
+        subcategory,
+        cost,
+        TIMESTAMPDIFF(MONTH, MIN(order_date), MAX(order_date)) AS lifespan,
+        MAX(order_date) AS last_sale_date,
+        COUNT(DISTINCT order_number) AS total_orders,
+        COUNT(DISTINCT customer_key) AS total_customers,
+        SUM(sales_amount) AS total_sales,
+        SUM(quantity) AS total_quantity,
+        ROUND(AVG(CASE WHEN quantity = 0 THEN NULL ELSE sales_amount / quantity END), 1) AS avg_selling_price
+    FROM base_query
+    GROUP BY
+        product_key,
+        product_name,
+        category,
+        subcategory,
+        cost
+)
+
+-- Step 3: Final KPIs and segmentation
+SELECT 
+    product_key,
+    product_name,
+    category,
+    subcategory,
+    cost,
+    last_sale_date,
+    TIMESTAMPDIFF(MONTH, last_sale_date, CURDATE()) AS recency_in_months,
+
+    CASE
+        WHEN total_sales > 50000 THEN 'High-Performer'
+        WHEN total_sales >= 10000 THEN 'Mid-Range'
+        ELSE 'Low-Performer'
+    END AS product_segment,
+
+    lifespan,
+    total_orders,
+    total_sales,
+    total_quantity,
+    total_customers,
+    avg_selling_price,
+
+    -- AOR: Average Order Revenue
+    CASE 
+        WHEN total_orders = 0 THEN 0
+        ELSE total_sales / total_orders
+    END AS avg_order_revenue,
+
+    -- Average Monthly Revenue
+    CASE
+        WHEN lifespan = 0 THEN total_sales
+        ELSE total_sales / lifespan
+    END AS avg_monthly_revenue
+
+FROM product_aggregations;
 
