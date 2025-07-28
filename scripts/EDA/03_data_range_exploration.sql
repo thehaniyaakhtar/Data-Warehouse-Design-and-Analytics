f8335d827de48f92180988165f0aed0da1674db3
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:12:23 +0530

    Update 03_data_range_exploration.sql

--- scripts/EDA/03_data_range_exploration.sql
+++ scripts/EDA/03_data_range_exploration.sql
@@ -1,1 +1,20 @@
+SELECT * 
+FROM gold_fact_sales
+;
 
+SELECT *
+FROM gold_dim_customers
+;
+
+SELECT 
+	MIN(order_date) AS first_order_date,
+    MAX(order_date) AS last_order_date,
+    TIMESTAMPDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_range_months
+FROM gold_fact_sales;
+
+SELECT 
+	MIN(birthdate) AS oldest_birthdate,
+    TIMESTAMPDIFF(YEAR, MIN(birthdate), CURDATE()) AS oldest_age,
+    MAX(birthdate) AS yougest_birthdate,
+    TIMESTAMPDIFF(YEAR, MAX(birthdate), CURDATE()) AS youngest_age
+FROM gold_dim_customers;
