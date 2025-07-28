a9125d310ddc3dbbc47b9cd08565067c96b3634a
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:23:19 +0530

    Update 02_dimensions_exploration.sql

--- scripts/EDA/02_dimensions_exploration.sql
+++ scripts/EDA/02_dimensions_exploration.sql
@@ -1,3 +1,17 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
+
+Table Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/
+
 USE DataWarehouseAnalytics;
 
 SELECT * 
