c7c989aff182bd066c38915bcc385fcad7affe2b
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:39:07 +0530

    Update 13_report_products.sql

--- scripts/EDA/13_report_products.sql
+++ scripts/EDA/13_report_products.sql
@@ -1,3 +1,29 @@
+/*
+===============================================================================
+Product Report
+===============================================================================
+Purpose:
+    - This report consolidates key product metrics and behaviors.
+
+Highlights:
+    1. Gathers essential fields such as product name, category, subcategory, and cost.
+    2. Segments products by revenue to identify High-Performers, Mid-Range, or Low-Performers.
+    3. Aggregates product-level metrics:
+       - total orders
+       - total sales
+       - total quantity sold
+       - total customers (unique)
+       - lifespan (in months)
+    4. Calculates valuable KPIs:
+       - recency (months since last sale)
+       - average order revenue (AOR)
+       - average monthly revenue
+===============================================================================
+*/
+-- =============================================================================
+-- Create Report: gold.report_products
+-- =============================================================================
+
 DROP VIEW IF EXISTS gold_report_products;
 
 CREATE VIEW gold_report_products AS
