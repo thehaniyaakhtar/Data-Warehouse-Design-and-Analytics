caf0e6a6843969cd50b1b7958348e9c029083219
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:11:20 +0530

    Update 01_database_exploration.sql

--- scripts/EDA/01_database_exploration.sql
+++ scripts/EDA/01_database_exploration.sql
@@ -1,1 +1,20 @@
+SELECT 
+	TABLE_SCHEMA,
+    TABLE_NAME,
+    TABLE_TYPE
+FROM
+	information_schema.TABLES
+WHERE
+	TABLE_SCHEMA = 'datawarehouseanalytics'
+;
 
+SELECT 
+	COLUMN_NAME,
+    DATA_TYPE,
+    IS_NULLABLE,
+    CHARACTER_MAXIMUM_LENGTH
+FROM
+	information_schema.COLUMNS
+WHERE TABLE_SCHEMA = 'datawarehouseanalytics'
+AND TABLE_NAME = 'gold_dim_customers'
+;
