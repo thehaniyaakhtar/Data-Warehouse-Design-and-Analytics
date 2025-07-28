dc0a682a36997e3670616c7007f84c5a8db8ab96
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:16:11 +0530

    Update 10_data_segmentation.sql

--- scripts/EDA/10_data_segmentation.sql
+++ scripts/EDA/10_data_segmentation.sql
@@ -1,1 +1,29 @@
+WITH customer_spending AS (
+	SELECT 
+		c.customer_key,
+        SUM(f.sales_amount) AS total_spending,
+        MIN(order_date) AS last_order,
+        MAX(order_date) AS last_order,
+        DATEDIFF(month, MIN(order_date), MAX(order_date)) AS lifespan
+        
+	FROM gold_fact_sales f
+	JOIN gold_dim_customers c
+		ON f.customer_key = c.customer_key
+	GROUP BY c.customer_key
+    )
 
+SELECT 
+	customer_segment,
+    COUNT(customer_key) AS total_customers
+FROM (
+	SELECT 
+		customer_key,
+        CASE
+			WHEN lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
+            WHEN lifespan >= 12 AND total_spending <= 5000 THEN 'Regular'
+            ELSE 'New'
+        END AS customer_segment
+    FROM customer_spending
+) AS segmented_customers
+GROUP BY customer_segment
+ORDER BY total_customers DESC;
