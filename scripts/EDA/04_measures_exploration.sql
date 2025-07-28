46d7f8ebfa34f3873d821697e7851459efb351d9
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:13:41 +0530

    Update 04_measures_exploration.sql

--- scripts/EDA/04_measures_exploration.sql
+++ scripts/EDA/04_measures_exploration.sql
@@ -1,1 +1,51 @@
+SELECT *
+FROM gold.fact_sales;
+
+SELECT SUM(sales_amount) AS Total_Sales
+FROM gold.fact_sales
+;
+
+SELECT SUM(quantity)
+FROM gold.fact_sales
+;
+
+SELECT AVG(price) as avg_price
+FROM gold.fact_sales
+;
+
+SELECT COUNT(DISTINCT order_number) AS total_orders
+FROM gold.fact_sales
+;
+
+SELECT COUNT(DISTINCT customer_key) AS total_customers
+FROM gold_dim_customers
+;
+
+SELECT 'Total Sales' AS metric, SUM(sales_amount) AS value 
+FROM gold_fact_sales
+
+UNION ALL
+
+SELECT 'Total Quantity', SUM(quantity) 
+FROM gold_fact_sales
+
+UNION ALL
+
+SELECT 'Average Price', AVG(price)
+FROM gold_fact_sales
+
+UNION ALL
+
+SELECT 'Total Orders', COUNT(DISTINCT order_number)
+FROM gold_fact_sales
+
+UNION ALL
+
+SELECT 'Total Products', COUNT(DISTINCT product_key) 
+FROM gold_fact_sales
+
+UNION ALL
+
+SELECT 'Total Customers', COUNT(*) 
+FROM gold.dim_customers;
 
