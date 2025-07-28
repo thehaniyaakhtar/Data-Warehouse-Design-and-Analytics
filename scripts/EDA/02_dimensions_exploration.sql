82fda97f4a8c82515696698715045b1efa1e4a40
Haniya Akhtar <thehaniyaakhtar@gmail.com>
2025-07-28 22:11:49 +0530

    Update 02_dimensions_exploration.sql

--- scripts/EDA/02_dimensions_exploration.sql
+++ scripts/EDA/02_dimensions_exploration.sql
@@ -1,1 +1,16 @@
+USE DataWarehouseAnalytics;
 
+SELECT * 
+FROM gold_dim_customers;
+
+SELECT DISTINCT country
+FROM gold_dim_customers
+ORDER BY country;
+
+SELECT * 
+FROM gold_dim_products;
+
+SELECT DISTINCT category, subcategory, product_name
+FROM gold_dim_products
+ORDER BY category, subcategory
+;
